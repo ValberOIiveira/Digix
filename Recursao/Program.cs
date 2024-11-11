bd799e3d118f612ee69f4cd1dfa92f216ff9044b
VALBER OLIVEIRA DA CONCEIÇÃO <valberoliveira789@gmail.com>
2024-11-11 15:56:32 -0400

    feat: adicionar aula de recursao

---
+++ Recursao/Program.cs
@@ -0,0 +1,37 @@
+﻿// namespace recursao{
+//     public class Program {
+//         public static void Main() {
+//             //Variavel que pode receber qual tipo de dado
+//             // var nome = "C#";
+
+
+//             // System.Console.WriteLine(nome);
+//             // System.Console.WriteLine(nome.GetType());
+
+
+//             //Criando lista var
+//             var list = new List<int> { 1, 2, 3, 4 };
+
+//             foreach (var num in list)
+//             {
+//                 // System.Console.WriteLine(num);
+//             }
+
+
+//             var dictionary = new Dictionary<string, int>
+//             {
+//                 {"C#", 1},
+//                 {"Python", 2},
+//                 {"Go", 3},
+//                 {"SQL", 4}
+//             };
+
+
+//             foreach (var dic in dictionary)
+//             {
+//                 System.Console.WriteLine($"{dic.Key} {dic.Value}");
+//             }
+
+//         }
+//     }
+// }
---
+++ Recursao/Ref.cs
@@ -0,0 +1,27 @@
+// using System;
+
+// namespace recursao
+// {
+//     public class Ref
+//     {
+
+//         static void Quadrado(ref int x)
+//         {
+//             x = x * x;
+//         }
+//         public static void Main()
+//         {
+
+//             var x = 10;
+//             System.Console.WriteLine(x);
+
+//             Quadrado(ref x);
+
+//             System.Console.WriteLine(x);
+
+
+//         }
+//     }
+// }
+
+
